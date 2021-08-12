29ba1b8b8ed943a6d540707ccc2cdcc7d87c153e
Kim Diep <kim.diep.techno@gmail.com>
2021-08-12 17:32:32 +0100

    Simplifies test setup to setup @beforeeach

--- src/test/java/BubbleTeaOrderRequestServiceSpyTest.java
+++ src/test/java/BubbleTeaOrderRequestServiceSpyTest.java
@@ -9,12 +9,12 @@ import testhelper.DummySimpleLogger;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-public class BubbleTeaOrderRequestServiceTest {
+public class BubbleTeaOrderRequestServiceSpyTest {
 
     private DebitCard testDebitCard;
     private PaymentDetails paymentDetails;
     private DummySimpleLogger dummySimpleLogger;
-    private BubbleTeaMessenger mockMessenger;
+    private BubbleTeaMessenger spiedMessenger;
     private BubbleTeaOrderService bubbleTeaOrderService;
 
     @BeforeEach
@@ -22,22 +22,22 @@ public class BubbleTeaOrderRequestServiceTest {
         testDebitCard = new DebitCard("0123456789");
         paymentDetails = new PaymentDetails("hello kitty", "sanrio puroland", testDebitCard);
         dummySimpleLogger = new DummySimpleLogger();
-        mockMessenger = mock(BubbleTeaMessenger.class);
-        bubbleTeaOrderService = new BubbleTeaOrderService(dummySimpleLogger, mockMessenger);
+        spiedMessenger = spy(new BubbleTeaMessenger(dummySimpleLogger));
+        bubbleTeaOrderService = new BubbleTeaOrderService(dummySimpleLogger, spiedMessenger);
     }
 
     @Test
     public void create_bubble_tea_order_request_when_createOrderRequest_is_called() {
 
         //Arrange
-        BubbleTea bubbleTea = new BubbleTea(BubbleTeaTypeEnum.OolongMilkTea, 4.5);
+        BubbleTea bubbleTea = new BubbleTea(BubbleTeaTypeEnum.MatchaMilkTea, 6.78);
         BubbleTeaRequest bubbleTeaRequest = new BubbleTeaRequest(paymentDetails, bubbleTea);
 
         BubbleTeaOrderRequest expectedResult = new BubbleTeaOrderRequest(
                 "hello kitty",
                 "sanrio puroland",
                 "0123456789",
-                BubbleTeaTypeEnum.OolongMilkTea);
+                BubbleTeaTypeEnum.MatchaMilkTea);
 
         //Act
         BubbleTeaOrderRequest result = bubbleTeaOrderService.createOrderRequest(bubbleTeaRequest);
@@ -48,9 +48,9 @@ public class BubbleTeaOrderRequestServiceTest {
         assertEquals(expectedResult.getDebitCardDigits(), result.getDebitCardDigits());
         assertEquals(expectedResult.getBubbleTeaType(), result.getBubbleTeaType());
 
-        //Verify Mock was called with BubbleTeaOrderRequest result
-        verify(mockMessenger).sendBubbleTeaOrderRequestEmail(result);
-        verify(mockMessenger, times(1)).sendBubbleTeaOrderRequestEmail(result);
+        //Check the spied messenger was called with BubbleTeaOrderRequest result
+        verify(spiedMessenger).sendBubbleTeaOrderRequestEmail(result);
+        verify(spiedMessenger, times(1)).sendBubbleTeaOrderRequestEmail(result);
     }
 
 }
