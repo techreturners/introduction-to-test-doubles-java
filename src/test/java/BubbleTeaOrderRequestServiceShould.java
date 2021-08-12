8814cf8f2e653d3943675715f30207da4669f8d4
Kim Diep <kim.diep.techno@gmail.com>
2021-08-12 12:34:37 +0100

    Adds example for mock vs. spy for BubbleTeaMessenger

--- src/test/java/BubbleTeaOrderRequestServiceShould.java
+++ src/test/java/BubbleTeaOrderRequestServiceShould.java
@@ -42,4 +42,39 @@ public class BubbleTeaOrderRequestServiceShould {
         verify(mockMessenger, times(1)).sendBubbleTeaOrderRequestEmail(result);
     }
 
+    @Test
+    public void create_bubble_tea_order_request_and_send_email_when_createOrderRequest_is_called() {
+
+        //Arrange
+        DebitCard testDebitCard = new DebitCard("0123456789");
+        PaymentDetails paymentDetails = new PaymentDetails("hello kitty", "sanrio puroland", testDebitCard);
+        BubbleTea bubbleTea = new BubbleTea(BubbleTeaTypeEnum.OolongMilkTea, 4.5);
+        BubbleTeaRequest bubbleTeaRequest = new BubbleTeaRequest(paymentDetails, bubbleTea);
+        DummySimpleLogger dummySimpleLogger = new DummySimpleLogger();
+        BubbleTeaMessenger spiedMessenger = spy(new BubbleTeaMessenger(dummySimpleLogger));
+
+        //Dummy Simple Logger injected into BubbleTeaOrderService
+        //mockMessenger injected into BubbleTeaOrderService
+        BubbleTeaOrderService bubbleTeaOrderService = new BubbleTeaOrderService(dummySimpleLogger, spiedMessenger);
+
+        //Act
+        BubbleTeaOrderRequest result = bubbleTeaOrderService.createOrderRequest(bubbleTeaRequest);
+
+        BubbleTeaOrderRequest expectedResult = new BubbleTeaOrderRequest(
+                "hello kitty",
+                "sanrio puroland",
+                "0123456789",
+                BubbleTeaTypeEnum.OolongMilkTea);
+
+        //Assert
+        assertEquals(expectedResult.getName(), result.getName());
+        assertEquals(expectedResult.getAddress(), result.getAddress());
+        assertEquals(expectedResult.getDebitCardDigits(), result.getDebitCardDigits());
+        assertEquals(expectedResult.getBubbleTeaType(), result.getBubbleTeaType());
+
+        //Check the spied messenger was called with BubbleTeaOrderRequest result
+        verify(spiedMessenger).sendBubbleTeaOrderRequestEmail(result);
+        verify(spiedMessenger, times(1)).sendBubbleTeaOrderRequestEmail(result);
+    }
+
 }
