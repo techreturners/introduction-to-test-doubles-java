1871de29cef71a595977fcbf89b6e48f7c43a7dc
Kim Diep <kim.diep.techno@gmail.com>
2022-01-18 11:56:07 +0000

    Fixes the name of the SimpleLogger interface

--- src/test/java/testhelper/DummySimpleLogger.java
+++ src/test/java/testhelper/DummySimpleLogger.java
@@ -1,8 +1,8 @@
 package testhelper;
 
-import com.techreturners.bubbleteaordersystem.model.SimpleLoggerImpl;
+import com.techreturners.bubbleteaordersystem.model.SimpleLogger;
 
-public class DummySimpleLogger implements SimpleLoggerImpl {
+public class DummySimpleLogger implements SimpleLogger {
 
     @Override
     public void addLoggingText(String loggingText) {
